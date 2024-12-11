5f478e1a88de4a32ec01a847caeed144085dcceb
dhivagar.rajendran <dhivagar.rajendran@centelon.com>
2024-12-11 08:40:58 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,19 @@
+ //javascript
+// #primitive data types
+
+// #number
+let age =24;
+age = 30;
+
+// #string
+let message = 'Hello';
+
+// #undefined
+let x;
+console.log(x);
+//Outptut:Undefined
+
+// #Null
+let y=null;
+console.log(y);
+//output: null
